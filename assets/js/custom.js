d16afc9254bc0876f801c08d4aef51b9e9722285
megharosejayan <megharose15@gmail.com>
2023-06-22 15:07:39 +0900

    Added: FAQ and Homepage update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,5 @@
 (function ($) {
-	
+
 	"use strict";
 
 	// Page loading animation
@@ -35,11 +35,6 @@
 	  }
 	});
 
-	var iframe = document.getElementById("my-iframe");
-	iframe.addEventListener("load", function() {
-	var playButton = iframe.querySelector(".ytp-large-play-button");
-	playButton.style.display = "none";
-	});
 
 
 	const Accordion = {
@@ -47,39 +42,31 @@
 		  // Expand the first item by default
 		  first_expanded: false,
 		  // Allow items to be toggled independently
-		  toggle: false,
+		  toggle: false
 		},
-	  
-		openAccordion: function (toggle, content) {
+
+		openAccordion: function(toggle, content) {
 		  if (content.children.length) {
 			toggle.classList.add("is-open");
 			let final_height = Math.floor(content.children[0].offsetHeight);
 			content.style.height = final_height + "px";
 		  }
 		},
-	  
-		closeAccordion: function (toggle, content) {
+
+		closeAccordion: function(toggle, content) {
 		  toggle.classList.remove("is-open");
 		  content.style.height = 0;
 		},
-	  
-		toggleAccordion: function (toggle, content) {
-		  if (toggle.classList.contains("is-open")) {
-			this.closeAccordion(toggle, content);
-		  } else {
-			this.openAccordion(toggle, content);
-		  }
-		},
-	  
-		init: function (el) {
+
+		init: function(el) {
 		  const _this = this;
-	  
+
 		  // Override default settings with classes
 		  let is_first_expanded = _this.settings.first_expanded;
 		  if (el.classList.contains("is-first-expanded")) is_first_expanded = true;
 		  let is_toggle = _this.settings.toggle;
 		  if (el.classList.contains("is-toggle")) is_toggle = true;
-	  
+
 		  // Loop through the accordion's sections and set up the click behavior
 		  const sections = el.getElementsByClassName("accordion");
 		  const all_toggles = el.getElementsByClassName("accordion-head");
@@ -88,140 +75,56 @@
 			const section = sections[i];
 			const toggle = all_toggles[i];
 			const content = all_contents[i];
-	  
+
 			// Click behavior
-			toggle.addEventListener("click", function (e) {
+			toggle.addEventListener("click", function(e) {
 			  if (!is_toggle) {
 				// Hide all content areas first
 				for (let a = 0; a < all_contents.length; a++) {
 				  _this.closeAccordion(all_toggles[a], all_contents[a]);
 				}
-	  
+
 				// Expand the clicked item
 				_this.openAccordion(toggle, content);
 			  } else {
-				_this.toggleAccordion(toggle, content);
+				// Toggle the clicked item
+				if (toggle.classList.contains("is-open")) {
+				  _this.closeAccordion(toggle, content);
+				} else {
+				  _this.openAccordion(toggle, content);
+				}
 			  }
 			});
-	  
-			// Close the accordion if clicked on itself
-			content.addEventListener("click", function (e) {
-			  if (is_toggle && toggle.classList.contains("is-open")) {
-				_this.closeAccordion(toggle, content);
-			  }
-			});
-	  
+
 			// Expand the first item
 			if (i === 0 && is_first_expanded) {
 			  _this.openAccordion(toggle, content);
 			}
 		  }
-		},
+		}
 	  };
-	  
-	  (function () {
+
+	  (function() {
 		// Initiate all instances on the page
 		const accordions = document.getElementsByClassName("accordions");
 		for (let i = 0; i < accordions.length; i++) {
 		  Accordion.init(accordions[i]);
 		}
 	  })();
-	  
 
-	
-	  
-
-
-
-	// const Accordion = {
-	// 	settings: {
-	// 	  // Expand the first item by default
-	// 	  first_expanded: false,
-	// 	  // Allow items to be toggled independently
-	// 	  toggle: false
-	// 	},
-  
-	// 	openAccordion: function(toggle, content) {
-	// 	  if (content.children.length) {
-	// 		toggle.classList.add("is-open");
-	// 		let final_height = Math.floor(content.children[0].offsetHeight);
-	// 		content.style.height = final_height + "px";
-	// 	  }
-	// 	},
-  
-	// 	closeAccordion: function(toggle, content) {
-	// 	  toggle.classList.remove("is-open");
-	// 	  content.style.height = 0;
-	// 	},
-  
-	// 	init: function(el) {
-	// 	  const _this = this;
-  
-	// 	  // Override default settings with classes
-	// 	  let is_first_expanded = _this.settings.first_expanded;
-	// 	  if (el.classList.contains("is-first-expanded")) is_first_expanded = true;
-	// 	  let is_toggle = _this.settings.toggle;
-	// 	  if (el.classList.contains("is-toggle")) is_toggle = true;
-  
-	// 	  // Loop through the accordion's sections and set up the click behavior
-	// 	  const sections = el.getElementsByClassName("accordion");
-	// 	  const all_toggles = el.getElementsByClassName("accordion-head");
-	// 	  const all_contents = el.getElementsByClassName("accordion-body");
-	// 	  for (let i = 0; i < sections.length; i++) {
-	// 		const section = sections[i];
-	// 		const toggle = all_toggles[i];
-	// 		const content = all_contents[i];
-  
-	// 		// Click behavior
-	// 		toggle.addEventListener("click", function(e) {
-	// 		  if (!is_toggle || !toggle.classList.contains("is-open")) {
-	// 			// Hide all content areas first
-	// 			for (let a = 0; a < all_contents.length; a++) {
-	// 			  _this.closeAccordion(all_toggles[a], all_contents[a]);
-	// 			}
-  
-	// 			// Expand the clicked item
-	// 			_this.openAccordion(toggle, content);
-	// 		  } else {
-	// 			_this.closeAccordion(toggle, content);
-	// 			// Toggle the clicked item
-	// 			// if (toggle.classList.contains("is-open")) {
-	// 			//   _this.closeAccordion(toggle, content);
-	// 			// } else {
-	// 			//   _this.openAccordion(toggle, content);
-	// 			// }
-	// 		  }
-	// 		});
-  
-	// 		// Expand the first item
-	// 		if (i === 0 && is_first_expanded) {
-	// 		  _this.openAccordion(toggle, content);
-	// 		}
-	// 	  }
-	// 	}
-	//   };
-  
-	//   (function() {
-	// 	// Initiate all instances on the page
-	// 	const accordions = document.getElementsByClassName("accordions");
-	// 	for (let i = 0; i < accordions.length; i++) {
-	// 	  Accordion.init(accordions[i]);
-	// 	}
-	//   })();
-  
 
 
 
 	$(document).on("click", ".naccs .menu div", function() {
 		var numberIndex = $(this).index();
-	
+
 		if (!$(this).is("active")) {
 			$(".naccs .menu div").removeClass("active");
 			$(".naccs ul li").removeClass("active");
-	
+
 			$(this).addClass("active");
 			$(".naccs ul").find("li:eq(" + numberIndex + ")").addClass("active");
-	
+
 			var listItemHeight = $(".naccs ul")
 				.find("li:eq(" + numberIndex + ")")
 				.innerHeight();
@@ -246,7 +149,7 @@
       }
 	});
 
-	
+
 
 	// Menu Dropdown Toggle
 	if($('.menu-trigger').length){
@@ -278,17 +181,17 @@
 
 	$(document).ready(function () {
 	    $(document).on("scroll", onScroll);
-	    
+
 	    //smoothscroll
 	    $('.scroll-to-section a[href^="#"]').on('click', function (e) {
 	        e.preventDefault();
 	        $(document).off("scroll");
-	        
+
 	        $('.scroll-to-section a').each(function () {
 	            $(this).removeClass('active');
 	        })
 	        $(this).addClass('active');
-	      
+
 	        var target = this.hash,
 	        menu = target;
 	       	var target = $(this.hash);
@@ -335,7 +238,7 @@
 		});
 	});
 
-	
+
 
 	const dropdownOpener = $('.main-nav ul.nav .has-sub > a');
 
@@ -375,7 +278,7 @@
     }
 
 
-	
+
 
 
 })(window.jQuery);
